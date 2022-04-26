7a52a7fa05786da144b18c0e92425f74ee3204cc
champix56 <desorbaix@free.fr>
2022-04-26 16:49:20 +0200

    gestion de cycle de vie et d'etat

--- src/App/App.js
+++ src/App/App.js
@@ -1,21 +1,44 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
 import Button from "./components/Button/Button";
+import "./App.css";
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      compteur: 0,
+      uneData: "Demat breizh",
+    };
+  }
+  componentDidMount() {
+    console.log("le composant est monté et pret a servir", arguments);
+  }
+  componentDidUpdate(prevProps, prevState) {
+    console.log(prevProps, this.props);
+    console.log(
+      "%c%s",
+      "font-size:24pt;color:red;font-weight:900",
+      "--PREV-----------NEW--"
+    );
+    console.log(prevState, this.state);
+  }
+  render() {
+    return (
+      <div className="App" data-testid="App">
+        <div>Valeur du compteur : {this.state.compteur}</div>
 
-function App() {
-  return (
-    <div>
-      <Button text="Mon button" />
-      <Button bgColor="skyblue">Un button</Button>
-      <Button color="magenta"><div>ABc</div></Button>
-      <Button>
-        <img src="https://cdn1.iconfinder.com/data/icons/photo-stickers-words/128/word_16-256.png" />
-        <div>helo</div>
-      </Button>
-      <Button />
-    </div>
-  );
+        <Button
+          bgColor="skyblue"
+          onButtonClicked={() => {
+            this.setState({ compteur: this.state.compteur + 1 });
+            console.log(this.state.compteur);
+          }}
+        >
+          +1
+        </Button>
+        <Button bgColor="tomato">-1</Button>
+      </div>
+    );
+  }
 }
 
 export default App;
