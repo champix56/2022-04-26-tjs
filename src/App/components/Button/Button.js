32fe3c189bafede69bf90b001751c955a8e47cc3
champix56 <desorbaix@free.fr>
2022-04-26 15:05:30 +0200

    gestion du hook d'effet

--- src/App/components/Button/Button.js
+++ src/App/components/Button/Button.js
@@ -1,30 +1,39 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import style from "./Button.module.css";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
+
 
 const Button = (props) => {
   const [isClicked, setClicked] = useState(false);
+  useEffect(() => {
+    console.log("updt du state", isClicked);
+    if (isClicked) {
+      setTimeout(() => {
+          setClicked(false);
+      }, 1000);
+    }
+  }, [isClicked]);
   return (
     <button
       className={style.Button}
       style={{ color: props.color, backgroundColor: props.bgColor }}
-      onClick={(evt)=>{
-           // console.log(evt);
-           setClicked(!isClicked);
-           console.log("value de isClicked:",isClicked);
+      onClick={(evt) => {
+        // console.log(evt);
+        setClicked(true);
+        //    console.log("value de isClicked:",isClicked);
       }}
     >
       {props.children}
-      <br/>
+      <br />
       {isClicked.toString()}
     </button>
   );
 };
-Button.propTypes={
-    bgColor: PropTypes.string.isRequired,
-    color: PropTypes.string.isRequired,
-}
-Button.defaultProps={
-    bgColor:'grey'
-}
+Button.propTypes = {
+  bgColor: PropTypes.string.isRequired,
+  color: PropTypes.string.isRequired,
+};
+Button.defaultProps = {
+  bgColor: "grey",
+};
 export default Button;
