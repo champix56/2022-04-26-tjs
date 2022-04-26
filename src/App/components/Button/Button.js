a7c7e843fc7eb8d4b54504360e8f6b5abb49d29a
champix56 <desorbaix@free.fr>
2022-04-26 14:52:31 +0200

    useage d'etat dans function

--- src/App/components/Button/Button.js
+++ src/App/components/Button/Button.js
@@ -1,14 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import style from "./Button.module.css";
 import PropTypes from 'prop-types';
+
 const Button = (props) => {
-  console.log(props);
+  const [isClicked, setClicked] = useState(false);
   return (
     <button
       className={style.Button}
       style={{ color: props.color, backgroundColor: props.bgColor }}
+      onClick={(evt)=>{
+           // console.log(evt);
+           setClicked(!isClicked);
+           console.log("value de isClicked:",isClicked);
+      }}
     >
       {props.children}
+      <br/>
+      {isClicked.toString()}
     </button>
   );
 };
