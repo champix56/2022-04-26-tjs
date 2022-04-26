52850ca49f1af6deee2d80b2e4ea862e480487b3
champix56 <desorbaix@free.fr>
2022-04-26 14:23:01 +0200

    gestion d'envoi de props

--- src/App/App.js
+++ src/App/App.js
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Button from "./components/Button/Button";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Button text="Mon button" />
+      <Button bgColor="skyblue">Un button</Button>
+      <Button color="magenta"><div>ABc</div></Button>
+      <Button>
+        <img src="https://cdn1.iconfinder.com/data/icons/photo-stickers-words/128/word_16-256.png" />
+        <div>helo</div>
+      </Button>
+      <Button />
     </div>
   );
 }
