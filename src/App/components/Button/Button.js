415683d635e1353a67a6c3601eba9b31fe8b1eee
champix56 <desorbaix@free.fr>
2022-04-26 16:09:45 +0200

    gestion des events

--- src/App/components/Button/Button.js
+++ src/App/components/Button/Button.js
@@ -2,25 +2,37 @@ import React, { useEffect, useState } from "react";
 import style from "./Button.module.css";
 import PropTypes from "prop-types";
 
-
 const Button = (props) => {
   const [isClicked, setClicked] = useState(false);
+  const [isOver, setOver] = useState(false);
   useEffect(() => {
     console.log("updt du state", isClicked);
     if (isClicked) {
       setTimeout(() => {
-          setClicked(false);
-      }, 1000);
+        setClicked(false);
+      }, 300);
     }
   }, [isClicked]);
+  let className = style.Button;
+  if (isClicked) {
+    className += ' ' + style.clicked;
+  } else if (isOver) {
+    className +=' ' + style["over"];
+  }
   return (
     <button
-      className={`${style.Button}${isClicked?' '+style.clicked:''}`}
+      className={style['Button--lambda']}
       style={{ color: props.color, backgroundColor: props.bgColor }}
       onClick={(evt) => {
         // console.log(evt);
         setClicked(true);
         //    console.log("value de isClicked:",isClicked);
+      }}
+      onMouseEnter={() => {
+        setOver(true);
+      }}
+      onMouseLeave={() => {
+        setOver(false);
       }}
     >
       {props.children}
