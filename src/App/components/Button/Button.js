1c01c15b76fea3bf3e9f047ad7a8a167a587493a
champix56 <desorbaix@free.fr>
2022-04-26 14:22:51 +0200

    gestion style du button

---
+++ src/App/components/Button/Button.js
@@ -0,0 +1,15 @@
+import React from "react";
+import style from "./Button.module.css";
+const Button = (props) => {
+  console.log(props);
+  return (
+    <button
+      className={style.Button}
+      style={{ color: props.color, backgroundColor: props.bgColor }}
+    >
+      Benjamin
+    </button>
+  );
+};
+
+export default Button;
