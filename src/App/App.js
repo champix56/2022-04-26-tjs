0607816358a28108b7c00b65a679c21985fb86d5
champix56 <desorbaix@free.fr>
2022-04-27 13:15:29 +0200

    meo de la navbar

--- src/App/App.js
+++ src/App/App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import FlexH from "./components/layout/FlexH/FlexH";
 import FlexW from "./components/layout/FlexW/FlexW";
 import MemeSvgViewer from "./components/ui/MemeSvgViewer/MemeSvgViewer";
+import Navbar from "./components/ui/Navbar/Navbar";
 const appInitialState = {
   meme: {
     text: "Le react c'est cool",
@@ -10,30 +11,33 @@ const appInitialState = {
     fontSize: 20,
     fontWeight: "900",
     color: "#ACB0A1",
-    imageId:0,
-    underline:true,
-    italic:false,
-    name:'mon 1er meme'
+    imageId: 1,
+    underline: true,
+    italic: false,
+    name: "mon 1er meme",
   },
-  images:[
+  images: [
     {
-      id:0,
-      name:'image',
-      url:'futurama1.jpg',
-      w:1200,
-      h:675
-    }
-  ]
+      id: 0,
+      name: "image",
+      url: "futurama1.jpg",
+      w: 1200,
+      h: 675,
+    },
+  ],
 };
 function App(props) {
   const [state, setstate] = useState(appInitialState);
   return (
     <div className="App" style={{ height: "90vh" }}>
       <div>Header</div>
-      <div>Nav</div>
+      <Navbar/>
       <FlexH>
         <FlexW>
-          <MemeSvgViewer meme={state.meme} image={}/>
+          <MemeSvgViewer
+            meme={state.meme}
+            image={state.images.find((img) => img.id === state.meme.imageId)}
+          />
           <div>Editor</div>
         </FlexW>
       </FlexH>
