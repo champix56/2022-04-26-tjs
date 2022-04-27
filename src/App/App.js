7ec1a2f4f35b17e55dd3049676667fe04064a181
champix56 <desorbaix@free.fr>
2022-04-27 11:03:47 +0200

    MEO

--- src/App/App.js
+++ src/App/App.js
@@ -1,44 +1,20 @@
-import React from "react";
-import Button from "./components/Button/Button";
-import "./App.css";
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      compteur: 0,
-      uneData: "Demat breizh",
-    };
-  }
-  componentDidMount() {
-    console.log("le composant est monté et pret a servir", arguments);
-  }
-  componentDidUpdate(prevProps, prevState) {
-    console.log(prevProps, this.props);
-    console.log(
-      "%c%s",
-      "font-size:24pt;color:red;font-weight:900",
-      "--PREV-----------NEW--"
-    );
-    console.log(prevState, this.state);
-  }
-  render() {
-    return (
-      <div className="App" data-testid="App">
-        <div>Valeur du compteur : {this.state.compteur}</div>
+import React from 'react'
+import FlexH from './components/layout/FlexH/FlexH'
+import FlexW from './components/layout/FlexW/FlexW'
 
-        <Button
-          bgColor="skyblue"
-          onButtonClicked={() => {
-            this.setState({ compteur: this.state.compteur + 1 });
-            console.log(this.state.compteur);
-          }}
-        >
-          +1
-        </Button>
-        <Button bgColor="tomato">-1</Button>
-      </div>
-    );
-  }
+function App(props) {
+  return (
+    <div className="App" style={{height:'90vh'}}>
+      <div>Header</div>
+      <div>Nav</div>
+      <FlexH>
+        <FlexW>
+          <div>Viewer</div>
+          <div>Editor</div>
+        </FlexW>
+      </FlexH>
+      <div>Footer</div>
+    </div>
+  )
 }
-
-export default App;
+export default App
---
+++ src/App/components/ui/Navbar/Navbar.js
@@ -0,0 +1,20 @@
+import React, { useEffect, useState } from "react";
+import PropTypes from "prop-types";
+import style from "./Navbar.js";
+export const navbarInitialState = {};
+const Navbar = (props) => {
+  const [state, setstate] = useState(navbarInitialState);
+  useEffect(() => {
+    
+  }, [state])
+  return (
+    <div className={style.Navbar} data-testid="Navbar">
+      navbar
+    </div>
+  );
+};
+
+Navbar.propTypes = {};
+Navbar.defaultProps = {};
+
+export default Navbar;
