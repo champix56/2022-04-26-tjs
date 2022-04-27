3e00f7cb7b1161798a3485d4990b31b42732a162
champix56 <desorbaix@free.fr>
2022-04-27 11:27:05 +0200

    mise en place d'un etat

--- src/App/App.js
+++ src/App/App.js
@@ -1,10 +1,33 @@
-import React from 'react'
-import FlexH from './components/layout/FlexH/FlexH'
-import FlexW from './components/layout/FlexW/FlexW'
-
+import React, { useState } from "react";
+import FlexH from "./components/layout/FlexH/FlexH";
+import FlexW from "./components/layout/FlexW/FlexW";
+const appInitialState = {
+  meme: {
+    text: "Le react c'est cool",
+    x: 20,
+    y: 20,
+    fontSize: 20,
+    fontWeight: "900",
+    color: "#ACB0A1",
+    imageId:0,
+    underline:true,
+    italic:false,
+    name:'mon 1er meme'
+  },
+  images:[
+    {
+      id:0,
+      name:'image',
+      url:'',
+      w:1280,
+      h:720
+    }
+  ]
+};
 function App(props) {
+  const [state, setstate] = useState(appInitialState);
   return (
-    <div className="App" style={{height:'90vh'}}>
+    <div className="App" style={{ height: "90vh" }}>
       <div>Header</div>
       <div>Nav</div>
       <FlexH>
@@ -15,6 +38,6 @@ function App(props) {
       </FlexH>
       <div>Footer</div>
     </div>
-  )
+  );
 }
-export default App
+export default App;
