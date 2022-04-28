825f68851501b78f1aab0d5282f9037e06e71549
champix56 <desorbaix@free.fr>
2022-04-28 15:10:55 +0200

    format link

--- src/App/App.js
+++ src/App/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route,Routes,Link} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import FlexH from "./components/layout/FlexH/FlexH";
 import FlexW from "./components/layout/FlexW/FlexW";
 import { ConnectedMemeForm } from "./components/MemeForm/MemeForm";
@@ -10,24 +10,26 @@ import Navbar from "./components/ui/Navbar/Navbar";
 function App(props) {
   return (
     <div className="App" style={{ height: "90vh" }}>
-    <Router>
-      <div><Link to="/thumbnail">thumbnail</Link> | <Link to="/editor">New</Link></div>
-      <Navbar />
-      <FlexH>
-        <Routes>
-          <Route path="/thumbnail" element={<MemeThumbnail />} />
-          <Route
-            path="/editor"
-            element={
-              <FlexW>
-                <MemeSvgViewer />
-                <ConnectedMemeForm />
-              </FlexW>
-            }
-          />
-        </Routes>
-      </FlexH>
-      <div>Footer</div>
+      <Router>
+        <div style={{ textAlign: "center" }}>
+          <Link to="/thumbnail">thumbnail</Link> | <Link to="/editor">New</Link>
+        </div>
+        <Navbar />
+        <FlexH>
+          <Routes>
+            <Route path="/thumbnail" element={<MemeThumbnail />} />
+            <Route
+              path="/editor"
+              element={
+                <FlexW>
+                  <MemeSvgViewer />
+                  <ConnectedMemeForm />
+                </FlexW>
+              }
+            />
+          </Routes>
+        </FlexH>
+        <div>Footer</div>
       </Router>
     </div>
   );
