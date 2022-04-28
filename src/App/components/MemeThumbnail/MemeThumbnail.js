d705e1150c650d33c9a9aae95ed776e76d79a08b
champix56 <desorbaix@free.fr>
2022-04-28 14:26:11 +0200

    MEmeThumbnail

---
+++ src/App/components/MemeThumbnail/MemeThumbnail.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { connect } from "react-redux";
+import MemeSvgViewer from "../ui/MemeSvgViewer/MemeSvgViewer";
+export const MemeThumbnail = (props) => {
+  return (
+    <div className={StyleSheet.MemeThumbnail} test-id="MemeThumbnail">
+      {props.memes.map((e, i) => (
+        <MemeSvgViewer
+          key={`th${i}`}
+          meme={e}
+          image={props.images.find((img) => img.id === e.imageId)}
+        />
+      ))}
+    </div>
+  );
+};
+
+const mapStateToProps = (state, props) => ({ ...props, ...state.ressources });
+
+const mapDispatchToProps = () => {};
+
+export default connect(mapStateToProps, mapDispatchToProps)(MemeThumbnail);
