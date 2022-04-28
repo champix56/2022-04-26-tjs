cea17eab37cdb3c11f6150a403c0b154024033a0
champix56 <desorbaix@free.fr>
2022-04-28 15:06:14 +0200

    ajout de route et de context de router

--- src/App/App.js
+++ src/App/App.js
@@ -1,25 +1,34 @@
 import React from "react";
+import { BrowserRouter as Router, Route,Routes,Link} from 'react-router-dom'
 import FlexH from "./components/layout/FlexH/FlexH";
 import FlexW from "./components/layout/FlexW/FlexW";
-import {ConnectedMemeForm} from "./components/MemeForm/MemeForm";
+import { ConnectedMemeForm } from "./components/MemeForm/MemeForm";
 import MemeSvgViewer from "./components/ui/MemeSvgViewer/ConnectedMemeSvgViewer";
-import  MemeThumbnail  from "./components/ui/MemeThumbnail/MemeThumbnail";
+import MemeThumbnail from "./components/ui/MemeThumbnail/MemeThumbnail";
 import Navbar from "./components/ui/Navbar/Navbar";
 
 function App(props) {
-
-   return (
+  return (
     <div className="App" style={{ height: "90vh" }}>
+    <Router>
       <div>Header</div>
       <Navbar />
-      <MemeThumbnail/>
       <FlexH>
-        <FlexW>
-          <MemeSvgViewer/>
-          <ConnectedMemeForm />
-        </FlexW>
+        <Routes>
+          <Route path="/thumbnail" element={<MemeThumbnail />} />
+          <Route
+            path="/editor"
+            element={
+              <FlexW>
+                <MemeSvgViewer />
+                <ConnectedMemeForm />
+              </FlexW>
+            }
+          />
+        </Routes>
       </FlexH>
       <div>Footer</div>
+      </Router>
     </div>
   );
 }
