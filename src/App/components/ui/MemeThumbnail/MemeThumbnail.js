20e0610b040e2de436913c79d817ddb9756d5d95
champix56 <desorbaix@free.fr>
2022-04-28 17:36:26 +0200

    ajout de liens pour l'edition + css

--- src/App/components/ui/MemeThumbnail/MemeThumbnail.js
+++ src/App/components/ui/MemeThumbnail/MemeThumbnail.js
@@ -1,16 +1,18 @@
 import React from "react";
 import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 import MemeSvgViewer from "../MemeSvgViewer/MemeSvgViewer";
 import style from './MemeThumbnail.module.scss'
 export const MemeThumbnail = (props) => {
   return (
     <div className={style.MemeThumbnail} test-id="MemeThumbnail">
       {props.memes.map((e, i) => (
+        <Link to={`/editor/${e.id}`} key={`th${i}`}>
         <MemeSvgViewer
-          key={`th${i}`}
           meme={e}
           image={props.images.find((img) => img.id === e.imageId)}
         />
+        </Link>
       ))}
     </div>
   );
