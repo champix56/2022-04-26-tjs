ea054c7346e02d6431d785cc70a7b6a6a16371f8
champix56 <desorbaix@free.fr>
2022-04-28 16:31:06 +0200

    mise en place de la connexion au route

--- src/App/App.js
+++ src/App/App.js
@@ -1,11 +1,13 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import { BrowserRouter as Router, Route, Routes, Link, useLocation,useParams} from "react-router-dom";
 import FlexH from "./components/layout/FlexH/FlexH";
 import FlexW from "./components/layout/FlexW/FlexW";
 import { ConnectedMemeForm } from "./components/MemeForm/MemeForm";
 import MemeSvgViewer from "./components/ui/MemeSvgViewer/ConnectedMemeSvgViewer";
 import MemeThumbnail from "./components/ui/MemeThumbnail/MemeThumbnail";
 import Navbar from "./components/ui/Navbar/Navbar";
+import store,{ACTIONS_CURRENT} from "./store/store";
 
 function App(props) {
   return (
@@ -19,6 +21,7 @@ function App(props) {
           <Routes>
             <Route path="/thumbnail" element={<MemeThumbnail />} />
             <Route path="/editor" element={<MemeEditor />} />
+            <Route path="/editor/:id" element={<MemeEditor />} />
           </Routes>
         </FlexH>
         <div>Footer</div>
@@ -26,12 +29,27 @@ function App(props) {
     </div>
   );
 }
-function MemeEditor() {
-  return (
+const mstp=(s)=>{
+    return {memes:s.ressources.memes}
+}
+const mdtp=(dispatch)=>{
+  return {
+    setCurrent:(meme)=>{
+        dispatch({type:ACTIONS_CURRENT.UPDATE_CURRENT,value:meme})
+    }
+  }
+}
+const MemeEditor=connect(mstp,mdtp)  ( (props)=>{
+  const id=useParams().id;
+  useEffect(() => {
+    props.setCurrent(props.memes.find(m=>m.id===Number(id)))
+  }, [props,id])
+ return (
     <FlexW>
       <MemeSvgViewer />
       <ConnectedMemeForm />
     </FlexW>
   );
-}
+})
+// const MemeEditor=connect(mstp,mdtp)(UnConnectedMemeEditor)
 export default App;
